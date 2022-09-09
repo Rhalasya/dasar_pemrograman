cf1e4729a4d3e9fd65476339989bc7e8bf3c2d41
Rhalasya <112567139+Rhalasya@users.noreply.github.com>
2022-09-09 13:06:49 +0700

    Add files via upload

---
+++ luas lingkaran.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	cout << "Menghitung Luas Lingkaran" << endl;
+	float r, luas;
+	
+	cout << "Input jari-jari lingkaran ;";
+	cin >> r;
+	luas = 3.14*r*r;
+	
+	cout << "luas lingkaran ="<< luas << endl;
+	
+	return 0;
+}
