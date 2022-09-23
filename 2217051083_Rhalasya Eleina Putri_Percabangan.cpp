c26a0dddfae2a73841cd74a67b5d8d0b183bc84e
Rhalasya <112567139+Rhalasya@users.noreply.github.com>
2022-09-23 09:15:34 +0700

    Add files via upload

---
+++ 2217051083_Rhalasya Eleina Putri_Percabangan.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a;
+	cout << "";
+	cin >> a;
+	
+	if (a%2==0){
+		cout <<a<< " bilangan genap";
+	}
+	else {
+		cout << a << " bilangan ganjil";
+	}
+}
